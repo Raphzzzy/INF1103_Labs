87ccad9e5e147c08ce948c642f06838069c23f34
Raphael? <raphaellee0307@gmail.com>
2026-09-04 10:28:20 +0800

    Dynamic Profile

--- Hello.py
+++ Hello.py
@@ -1,17 +1,9 @@
-print ("WOHOOOO")
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-username = ("Ben Dover")
-bio = ("yaoi enjoyer")
-followers = 67
-
-followers += 2
-print ("Day 1:", followers)
-
-followers += 20
-print ("Day 2:", followers)
-
-followers += 100
-print ("Day 3:", followers)
-
-
-
+print ("\nInstagram Profile")
+print ("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
